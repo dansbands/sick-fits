f997655a260f8c7658042946fc8f6c7ad0baf7af
dansbands <dansbands@hotmail.com>
2020-05-20 12:07:57 -0400

    built cart front end

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "584cfb3d2d17e973bd18";
+/******/ 	var hotCurrentHash = "fb9c741aa6ec8909af55";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
