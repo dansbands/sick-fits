319e136510fcd65f3ba1d0bd794350419f13ccce
dansbands <dansbands@hotmail.com>
2020-05-22 20:55:35 -0400

    optimistic delete

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c655abccc9607fd67cd6";
+/******/ 	var hotCurrentHash = "80d81bdb693c483d5ecc";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
