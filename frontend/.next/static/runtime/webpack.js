222211e514dd6c54abb73b32dfb19f073d939397
dansbands <dansbands@hotmail.com>
2020-05-25 00:25:25 -0400

    orders page

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b87193401a0b8d43af90";
+/******/ 	var hotCurrentHash = "13368006358212d4f0f1";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
