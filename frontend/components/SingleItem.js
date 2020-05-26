a95144b63513ca0f45f473410829ffafadf2020e
dansbands <dansbands@hotmail.com>
2020-05-26 19:14:51 -0400

    Mocking Apollo Queries

--- frontend/components/SingleItem.js
+++ frontend/components/SingleItem.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import gql from 'graphql-tag';
-import { Query } from 'react-apollo'
-import Error from './ErrorMessage'
-import styled from 'styled-components'
-import Head from 'next/head'
+import React from "react";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+import Error from "./ErrorMessage";
+import styled from "styled-components";
+import Head from "next/head";
 
 const SingleItemStyles = styled.div`
   max-width: 1200px;
   margin: 2rem auto;
-  box-shadow: ${props => props.theme.bs};
+  box-shadow: ${(props) => props.theme.bs};
   display: grid;
   grid-auto-columns: 1fr;
   grid-auto-flow: column;
@@ -35,33 +35,37 @@ const SINGLE_ITEM_QUERY = gql`
   }
 `;
 
-
 class SingleItem extends React.Component {
-  render () {
+  render() {
     return (
-      <Query query={SINGLE_ITEM_QUERY} variables={{
+      <Query
+        query={SINGLE_ITEM_QUERY}
+        variables={{
           id: this.props.id,
         }}
-        >
+      >
         {({ error, loading, data }) => {
-          if(error) return <Error error={error} />
-          if(loading) return <p>Loading...</p>
-          if(!data.item) return <p>No Item Found for {this.props.id}</p>
+          if (error) return <Error error={error} />;
+          if (loading) return <p>Loading...</p>;
+          if (!data.item) return <p>No Item Found for {this.props.id}</p>;
           const { item } = data;
-          return <SingleItemStyles>
-            <Head>
-              <title>Sick Fits | {item.title}</title>
-            </Head>
-            <img src={item.largeImage} alt={item.title} />
-            <div className="details">
-              <h2>{item.title}</h2>
-              <p>{item.description}</p>
-            </div>
-          </SingleItemStyles>
+          return (
+            <SingleItemStyles>
+              <Head>
+                <title>Sick Fits | {item.title}</title>
+              </Head>
+              <img src={item.largeImage} alt={item.title} />
+              <div className="details">
+                <h2>{item.title}</h2>
+                <p>{item.description}</p>
+              </div>
+            </SingleItemStyles>
+          );
         }}
       </Query>
-    )
+    );
   }
 }
 
 export default SingleItem;
+export { SINGLE_ITEM_QUERY };
