c245253bef22dccf4e54d308acaef0dfde9d5540
dansbands <dansbands@hotmail.com>
2020-05-28 13:59:49 -0400

    pagination tests

--- frontend/components/Pagination.js
+++ frontend/components/Pagination.js
@@ -1,11 +1,11 @@
 import React from "react";
 import gql from "graphql-tag";
 import { Query } from "react-apollo";
-import Head from 'next/head'
-import Link from 'next/link'
+import Head from "next/head";
+import Link from "next/link";
 import PaginationStyles from "./styles/PaginationStyles";
 import { perPage } from "../config";
-import Error from './ErrorMessage'
+import Error from "./ErrorMessage";
 
 const PAGINATION_QUERY = gql`
   query PAGINATION_QUERY {
@@ -21,34 +21,43 @@ const Pagination = (props) => (
   <Query query={PAGINATION_QUERY}>
     {({ data, loading, error }) => {
       if (loading) return <p>Loading...</p>;
-      if (error) return <Error error={error} />
+      if (error) return <Error error={error} />;
       const count = data.itemsConnection.aggregate.count;
       const pages = Math.ceil(count / perPage);
       const { page } = props;
       return (
-        <PaginationStyles>
+        <PaginationStyles data-test="pagination">
           <Head>
-            <title>Sick Fits! - Page {page} of {pages}</title>
+            <title>
+              Sick Fits! - Page {page} of {pages}
+            </title>
           </Head>
           <Link
             prefetch
             href={{
-              pathname: 'items',
-              query: { page: page - 1}
-            }}>
-            <a className="prev" aria-disabled={page <= 1}>← Prev</a>
+              pathname: "items",
+              query: { page: page - 1 },
+            }}
+          >
+            <a className="prev" aria-disabled={page <= 1}>
+              ← Prev
+            </a>
           </Link>
           <p>
-            Page {props.page} of {pages}!
+            Page {props.page} of
+            <span className="totalPages">{pages}</span>!
           </p>
           <p>{count} Items Total</p>
           <Link
             prefetch
             href={{
-              pathname: 'items',
-              query: { page: page + 1}
-            }}>
-            <a className="next" aria-disabled={page >= pages}>Next →</a>
+              pathname: "items",
+              query: { page: page + 1 },
+            }}
+          >
+            <a className="next" aria-disabled={page >= pages}>
+              Next →
+            </a>
           </Link>
         </PaginationStyles>
       );
@@ -57,3 +66,4 @@ const Pagination = (props) => (
 );
 
 export default Pagination;
+export { PAGINATION_QUERY };
